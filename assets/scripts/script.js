96f397fabd0af14e6bb0319deb3986c26e1a000f
ShroudLoaded <101376166+shroudloaded@users.noreply.github.com>
2023-06-06 21:57:11 +0530

    Update script.js
    Removed unnecessarycomments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,42 +1,36 @@
-// script.js
+// Social media links
+const socialLinksContainer = document.querySelector('.social-links');
 
-// Get all the social media links
-const socialLinks = document.querySelectorAll('.social-links a');
-
-// Attach a hover event listener to each link
-socialLinks.forEach(link => {
-  link.addEventListener('mouseover', () => {
-    link.style.transform = 'scale(1.1)';
-  });
-
-  link.addEventListener('mouseout', () => {
-    link.style.transform = 'scale(1)';
-  });
+socialLinksContainer.addEventListener('mouseover', function(event) {
+  if (event.target.matches('.social-links a')) {
+    event.target.classList.add('link-hovered');
+  }
 });
 
-// Get the header element
+socialLinksContainer.addEventListener('mouseout', function(event) {
+  if (event.target.matches('.social-links a')) {
+    event.target.classList.remove('link-hovered');
+  }
+});
+
+// Header font color animation
 const header = document.getElementById('dynamic-header');
-
-// Define an array of colors to cycle through
 const colors = ['#ff0000', '#00ff00', '#0000ff'];
-
-// Initialize a counter
 let colorIndex = 0;
 
-// Function to update the font color
 function updateFontColor() {
   header.style.color = colors[colorIndex];
   colorIndex = (colorIndex + 1) % colors.length;
+  requestAnimationFrame(updateFontColor);
 }
 
-// Call the function every 2 seconds (2000 milliseconds)
-setInterval(updateFontColor, 200);
+updateFontColor();
 
-// script.js
-
+// Popup functionality
 const popupOverlay = document.getElementById('popup-overlay');
 const popup = document.getElementById('popup');
-const closePopupBtn = document.getElementById('close-popup');
+const openButton = document.getElementById('open-button');
+const closeButton = document.querySelector('.close-button');
 
 function openPopup() {
   popupOverlay.style.display = 'block';
@@ -48,32 +42,16 @@ function closePopup() {
   popup.style.display = 'none';
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
   openPopup();
 });
 
-function openPopup() {
-  var popup = document.getElementById("popup");
-  popup.style.display = "block";
-}
+openButton.addEventListener('click', function() {
+  setTimeout(function() {
+    popup.style.display = 'block';
+  }, 10000);
+});
 
-function closePopup() {
-  var popup = document.getElementById("popup");
-  popup.style.display = "none";
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  var openButton = document.getElementById('open-button');
-  var popup = document.getElementById('popup');
-  var closeButton = document.querySelector('.close-button');
-
-  openButton.addEventListener('click', function() {
-    setTimeout(function() {
-      popup.style.display = 'block';
-    }, 10000); // Delay in milliseconds (e.g., 2000 for 2 seconds)
-  });
-
-  closeButton.addEventListener('click', function() {
-    popup.style.display = 'none';
-  });
+closeButton.addEventListener('click', function() {
+  popup.style.display = 'none';
 });
